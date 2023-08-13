76ae85211bfae925c50e6e451d71ae4ca8a7fe1c
Gary Topping <toppinggary@hotmail.com>
2023-08-14 02:20:56 +0900

    change

--- app/models/weapon.rb
+++ app/models/weapon.rb
@@ -1,3 +1,7 @@
 class Weapon < ApplicationRecord
   has_many :guesses, dependent: :destroy
+  has_many :weapons, through: :guesses
+  has_many :suspects, through: :guesses
+  validates :name, presence: true, uniqueness: true
+  has_one_attached :photo
 end
